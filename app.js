9181bcbd6501dc219c82e2c58f2417e9bb20dafd
Khai Truong <56820749+khaitruong922@users.noreply.github.com>
2021-10-04 15:51:51 +0700

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const cookieParser = require('cookie-parser')
 const appController = require('./core/app.controller')
 const User = require('./core/user/user.schema')
 const getKeyByValue = require('./helpers/getKeyByValue')
-const { Board, X, O } = require('./board')
+const { Board, X, O } = require('./Board')
 
 
 const PORT = process.env.PORT || 3000
