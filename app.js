db87250381c3f929a111ed5f15b2d0cca263626b
Khai Truong <56820749+khaitruong922@users.noreply.github.com>
2021-10-05 13:31:45 +0700

    Finish elo formula + preview elo + emit online players

--- app.js
+++ app.js
@@ -7,10 +7,12 @@ const morgan = require('morgan')
 const mongoose = require('mongoose')
 const socketio = require('socket.io')
 const cookieParser = require('cookie-parser')
+
 const appController = require('./core/app.controller')
 const User = require('./core/user/user.schema')
 const getKeyByValue = require('./helpers/getKeyByValue')
 const { Board, X, O } = require('./Board')
+const { calculateElo, calculateEloPreview } = require('./helpers/calculateElo')
 
 
 const PORT = process.env.PORT || 3000
@@ -51,6 +53,10 @@ io.on('connection', socket => {
         socket.emit('roomsChanged', { rooms })
     })
 
+    socket.on('requestPlayers', () => {
+        socket.emit('playersChanged', { players })
+    })
+
     socket.on('requestJoinLobby', async ({ playerId }) => {
         sockets[socketId] = playerId
         // Force disconnect on first socket if a user connects with 2 sockets
@@ -84,22 +90,30 @@ io.on('connection', socket => {
         player.socketId = targetSocketId
         players[playerId] = player
         io.to(targetSocketId).emit('joinLobby')
+        io.emit('playersChanged', { players })
         console.log(`Player ${player.username} has join the lobby!`)
     }
 
     function leaveLobby({ playerId }) {
         const player = players[playerId]
+
         // Leave room
         leaveRoom({ playerId, roomId: player?.roomId })
-        // Remove player from list
         console.log(`Player ${player?.username} has left the lobby!`)
+
+        // Remove player from list
         delete players[playerId]
+        io.emit('playersChanged', { players })
+
     }
 
     function joinRoom({ playerId, roomId }) {
-        if (roomId == null) return
         const room = rooms[roomId]
         if (!room) return
+        if (room.players.length > 1) {
+            socket.emit('joinRoomFailed', ({ message: "Room is already full!" }))
+            return
+        }
         const player = players[playerId]
         if (!player) return
         // Change data
@@ -110,36 +124,69 @@ io.on('connection', socket => {
         socket.join(roomId)
         socket.emit('joinRoom', { roomId })
         io.to(roomId).emit('playerJoinRoom', { roomId, player })
-        io.to(roomId).emit('roomPlayersChanged', { players: room.players, board: null })
+        io.to(roomId).emit('roomPlayersChanged', { players: room.players })
         io.emit('roomsChanged', { rooms })
-
+        previewElo({ roomId })
         console.log(`Player ${player.username} has entered room ${roomId}`)
     }
 
-    function leaveRoom({ playerId, roomId }) {
+    function previewElo({ roomId }) {
+        const room = rooms[roomId]
+        if (!room) return
+        // Calculate elo preview if 2 players in room
+        const { players: roomPlayers } = room
+        if (roomPlayers.length === 2) {
+            const [player1, player2] = roomPlayers
+            const { _id: p1, elo: r1 } = player1
+            const { _id: p2, elo: r2 } = player2
+            // Player 1 win player 2
+            const { high, low, draw } = calculateEloPreview({ r1, r2 })
+            const highEloPlayer = r1 > r2 ? player1 : player2
+            const lowEloPlayer = highEloPlayer === player1 ? player2 : player1
+            console.log('elopreview')
+            io.to(highEloPlayer.socketId).emit('eloPreview', { win: low, draw: -draw, lose: -high })
+            io.to(lowEloPlayer.socketId).emit('eloPreview', { win: high, draw: draw, lose: -low })
+        }
+    }
+
+    async function leaveRoom({ playerId, roomId }) {
         if (roomId == null) return
         const room = rooms[roomId]
         if (!room) return
         const player = players[playerId]
         // Change data
         if (player) player.roomId = null
-        room.ready = false
-        room.players = room.players.filter(p => String(p._id) !== playerId)
+
+        // Check if leave during game
+        const { board, players: roomPlayers } = room
+        if (board && roomPlayers.length === 2) {
+            const loser = roomPlayers.find(p => p._id === playerId)._id
+            const winner = roomPlayers.find(p => p._id !== playerId)._id
+            await win({ winner, loser, roomId })
+        }
+
+
+        // Remove player from room
+        room.players = room.players.filter(p => p._id !== playerId)
         if (room.players.length === 0) {
             delete rooms[roomId]
         }
+
         // Event
         socket.leave(roomId)
         socket.emit('leaveRoom')
+        io.to(roomId).emit('notReady')
         io.to(roomId).emit('playerLeaveRoom', { playerId, username: player.username })
         io.to(roomId).emit('roomPlayersChanged', { players: room.players })
+        io.to(roomId).emit('resetBoard')
         io.emit('roomsChanged', { rooms })
+        resetBoard({ roomId })
         console.log(`Player ${player?.username} has left room ${roomId}`)
     }
 
     function createRoom({ playerId }) {
         const roomId = currentRoomId
-        rooms[roomId] = { players: [] }
+        rooms[roomId] = { players: [], board: null }
         joinRoom({ playerId, roomId })
         currentRoomId++
     }
@@ -149,12 +196,6 @@ io.on('connection', socket => {
     })
 
     socket.on('joinRoom', ({ roomId, playerId }) => {
-        const room = rooms[roomId]
-        if (!room) return
-        if (room.players.length > 1) {
-            socket.emit('joinRoomFailed', ({ message: "Room is already full!" }))
-            return
-        }
         joinRoom({ playerId, roomId })
     })
 
@@ -196,43 +237,91 @@ io.on('connection', socket => {
         const room = rooms[roomId]
         if (!room) return
         const roomPlayers = room.players
-        if (players.length < 2) return
+        if (roomPlayers.length < 2) return
         console.log(`Move from player ${playerId} at row ${r} column ${c} in room ${roomId}`)
         const { board } = room
         const stone = board.getCurrentStone()
+        // If move is invalid then stop
         if (!board.addMove({ playerId, r, c })) return
         io.to(roomId).emit('move', { stone, r, c, })
         const socketId = board.currentTurn === roomPlayers[0]._id ? roomPlayers[0].socketId : roomPlayers[1].socketId
 
-        const { winner, loser } = board
+        const { winner, loser, player1, player2 } = board
         if (winner && loser) {
-            io.to(roomId).emit('playerWin', { playerId: winner, username: players[winner].username })
-            await calculateElo({ winner, loser })
-            io.emit('roomsChanged', { rooms })
-            io.to(roomId).emit('roomPlayersChanged', { players: room.players })
+            await win({ winner, loser, roomId })
             return
         }
         if (board.isDraw()) {
-            io.to(roomId).emit('draw')
+            await draw({ p1: player1, p2: player2 })
+            return
         }
         io.to(socketId).emit('yourTurn')
     })
 
-    async function calculateElo({ winner, loser }) {
+    async function draw({ p1, p2, roomId }) {
+        io.to(roomId).emit('draw')
+        await updateDrawElo({ p1, p2 })
+        updateRoomAfterGame({ roomId })
+    }
+
+    async function win({ winner, loser, roomId }) {
+        io.to(roomId).emit('playerWin', { playerId: winner, username: players[winner].username })
+        await updateElo({ winner, loser })
+        updateRoomAfterGame({ roomId })
+    }
+
+    function resetBoard({ roomId }) {
+        const room = rooms[roomId]
+        if (!room) return
+        room.board = null
+        io.to(roomId).emit('resetBoard')
+    }
+
+    function updateRoomAfterGame({ roomId }) {
+        const room = rooms[roomId]
+        if (!room) return
+
+        // Update room
+        room.board = null
+        previewElo({ roomId })
+        io.to(roomId).emit('roomPlayersChanged', { players: room.players })
+
+        // Update lobby
+        io.emit('playersChanged', { players })
+        io.emit('roomsChanged', { rooms })
+    }
+
+    async function updateElo({ winner, loser }) {
         const winnerPlayer = await User.findById(winner)
-        const loserPlayer = await User.findById(winner)
-        await User.findByIdAndUpdate(winner, { $inc: { elo: 10 } }, { new: true }).exec().then(doc => {
-            console.log(doc)
+        const loserPlayer = await User.findById(loser)
+        const { r1, r2 } = calculateElo({ r1: winnerPlayer.elo, r2: loserPlayer.elo, w: 1 })
+        await User.findByIdAndUpdate(winner, { $set: { elo: r1 } }, { new: true }).exec().then(doc => {
             const player = players[winner]
             player.elo = doc.elo
         })
-        await User.findByIdAndUpdate(loser, { $inc: { elo: -10 } }, { new: true }).exec().then(doc => {
-            console.log(doc)
+        await User.findByIdAndUpdate(loser, { $set: { elo: r2 } }, { new: true }).exec().then(doc => {
             const player = players[loser]
             player.elo = doc.elo
         })
     }
+
+    async function updateDrawElo({ p1, p2 }) {
+        const player1 = await User.findById(p1)
+        const player2 = await User.findById(p2)
+        const { r1, r2 } = calculateElo({ r1: player1.elo, r2: player2.elo, w: 0 })
+        await User.findByIdAndUpdate(player1, { $set: { elo: r1 } }, { new: true }).exec().then(doc => {
+            const player = players[p1]
+            player.elo = doc.elo
+        })
+        await User.findByIdAndUpdate(player2, { $set: { elo: r2 } }, { new: true }).exec().then(doc => {
+            const player = players[p2]
+            player.elo = doc.elo
+        })
+
+
+    }
 })
+
 server.listen(PORT, () => {
     console.log(`✔ Server is running on http://localhost:${PORT}`)
 })
---
+++ helpers/calculateElo.js
@@ -0,0 +1,42 @@
+const K = 30
+
+// Probability of rating 1 win against rating 2
+function probability(r1, r2) {
+    return 1 / (1 + 1 * Math.pow(10, (r2 - r1) / 400))
+}
+
+function calculateEloPreview({ r1, r2 }) {
+    const p1 = probability(r1, r2)
+    const p2 = probability(r2, r1)
+    const highP = Math.max(p1, p2)
+    const lowP = Math.min(p1, p2)
+    let high = Math.round(K * highP)
+    let low = Math.round(K * lowP)
+    let draw = Math.round(K * (0.5 - lowP))
+    return { high, low, draw }
+}
+
+function calculateElo({ r1, r2, w }) {
+    const p1 = probability(r1, r2)
+    const p2 = probability(r2, r1)
+    console.log(`Current elo: P1:${r1} P2:${r2}`)
+    let newR1 = r1
+    let newR2 = r2
+    if (w === 1) {
+        newR1 += K * (1 - p1)
+        newR2 += K * (0 - p2)
+    } else if (w === 2) {
+        newR1 += K * (0 - p1)
+        newR2 += K * (1 - p2)
+        // Draw
+    } else if (w === 0) {
+        newR1 += K * (0.5 - p1)
+        newR2 += K * (0.5 - p2)
+    }
+    newR1 = Math.round(newR1)
+    newR2 = Math.round(newR2)
+    console.log(`New elo: P1:${newR1} P2:${newR2}`)
+    return { r1: newR1, r2: newR2 }
+}
+
+module.exports = { calculateElo, calculateEloPreview }
